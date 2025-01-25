d2f6c572bca23288061c2e9292127c7839cc4dab
Francisco Giagnoni <franciscogiagnoni@gmail.com>
2025-01-25 13:29:46 -0500

    Update Project Day 3.sql

--- Project Day 3.sql
+++ Project Day 3.sql
@@ -1,18 +1,18 @@
--- Basado en la Dase de Datos netflixDB, realizar consultas que respondan a las siguientes preguntas:
--- 1) ¿Quién es el actor que ha participado en la mayor cantidad de series?
--- 2) ¿Cuál es la serie con rating de imdb promedio más alto?
--- 3) ¿Cuál es el episodio más largo?
+-- Based on the netflixDB database, perform queries that answer the following questions:
+-- 1) Who is the actor who has participated in the most series?
+-- 2) What is the series with the highest average imdb rating?
+-- 3) What is the longest episode?
 
--- 1) ¿Quién es el actor que ha participado en la mayor cantidad de series?
+--- 1) Who is the actor who has participated in the most series?
 SELECT actor_id, COUNT(serie_id) AS numero_de_series FROM actuaciones
 GROUP BY actor_id
 ORDER BY numero_de_series DESC;
 
--- 2) ¿Cuál es la serie con rating de imdb promedio más alto?
+-- 2) What is the series with the highest average imdb rating?
 SELECT serie_id, AVG(rating_imdb) AS rating_promedio FROM episodios
 GROUP BY serie_id
 ORDER BY rating_promedio DESC;
 
--- 3) ¿Cuál es el episodio más largo?
+-- 3) What is the longest episode?
 SELECT titulo, duracion FROM episodios
 ORDER BY duracion DESC;
