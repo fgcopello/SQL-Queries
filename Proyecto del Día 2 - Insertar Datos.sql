0e87b616283302a11c6f44e67cc6f52293581c31
Francisco Giagnoni <franciscogiagnoni@gmail.com>
2025-01-25 13:18:56 -0500

    Update Proyecto del Día 2 - Insertar Datos.sql

--- Proyecto del Día 2 - Insertar Datos.sql
+++ Proyecto del Día 2 - Insertar Datos.sql
@@ -1,16 +1,16 @@
--- Insertar datos en la tabla Desarrolladores
+-- Insert data on table 'Desarrolladores'
 INSERT INTO Desarrolladores (Nombre, Pais) VALUES 
 ('Naughty Dog', 'Estados Unidos'),
 ('Nintendo', 'Japón'),
 ('CD Projekt Red', 'Polonia');
 
--- Insertar datos en la tabla Plataformas
+-- Insert data on table 'Plataformas'
 INSERT INTO Plataformas (Nombre, Tipo) VALUES 
 ('PlayStation 4', 'Consola'),
 ('Nintendo Switch', 'Consola'),
 ('PC', 'Computadora');
 
--- Insertar datos en la tabla Juegos
+-- IInsert data on table 'Juegos'
 INSERT INTO Juegos (Titulo, AñoLanzamiento, DesarrolladorID, PlataformaID) VALUES 
 ('The Last of Us Part II', 2020, 1, 1),  -- Desarrollado por Naughty Dog para PlayStation 4
 ('The Legend of Zelda: Breath of the Wild', 2017, 2, 2),  -- Desarrollado por Nintendo para Nintendo Switch
